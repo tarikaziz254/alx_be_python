229e58d9defe67709f8ffea3b20a1ab580c436c5
tarik <tarikaziz2242@gmail.com>
2024-06-18 01:17:51 -0700

    addition,subtraction and multiplication of two numbers in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add_result = number1 + number2
+sub_result = number1 - number2
+mult_result = number2 * number2
+print(f"addition of {number1} and {number2} is {add_result})
+        print(f"subtraction of {number1} and {number2} is {sub_result})
+print(f"multiplication of {number1} and {number2} is {mult_result})
