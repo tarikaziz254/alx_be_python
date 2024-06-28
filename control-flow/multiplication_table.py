fa17393cd5f10fc6680f76fc8b01cfd68ab86851
tarik <tarikaziz2242@gmail.com>
2024-06-27 23:20:58 -0700

    updated product to number * i in this file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number=int(input("Enter a number to see its multiplication table:"))
 for i in range(1,11):
     product=number*1
-    print(f"{number} * {i} = {product}")
+    print(f"{number} * {i} = {number*i}")
