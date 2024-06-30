b84e68d6fadcb39b31374c76caa2cd6532bf177b
tarik <tarikaziz2242@gmail.com>
2024-06-30 01:40:24 -0700

    creating daily reminder using match case and if statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,19 +1,10 @@
-task=input("Enter your task:")
-priority=input("priority (high/medium/low)")
-time_bound=input("Is it time-bound? (yes/no):")
+task = input("Enter your task:")
+priority = input("Priority (high/medium/low):")
+time_bound = input("Is it time-bound? (yes/no):")
 match priority:
     case 'high':
         if time_bound == 'yes':
             print("Reminder: 'Finish project report' is a high priority task that requires immediate attention today!")
-        if time_bound == 'no':
-            print("Note: 'Read a book' is a low priority task. Consider completing it when you have free time.")
-    case 'medium':
-        if time_bound == 'yes':
-            print("Reminder: 'Finish project report' is a medium priority task that requires immediate attention today!")
-        if time_bound == 'no':
-            print("Note: 'Read a book' is a low priority task. Consider completing it when you have free time.")
     case 'low':
-        if time_bound == 'yes':
-            print("Reminder: 'Finish project report' is a low priority task but it has a time bound that requires immediate attention today!")
-        if time_bound == 'no':
+        if time_bound == 'low':
             print("Note: 'Read a book' is a low priority task. Consider completing it when you have free time.")
