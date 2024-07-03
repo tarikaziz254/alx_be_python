f191113b8d2a8d2a0dc519e61568f86c85a79a74
tarik <tarikaziz2242@gmail.com>
2024-07-03 07:24:03 -0700

    changed logic of the program from match case to if statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,20 +3,19 @@ def perform_operation():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operator = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    match operator:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "cannot divide by zero"
-        case _:
-            return "invalid input"
+    if operator == 'add':
+        return num1 + num2
+    elif operator == 'subtract':
+        return num1 - num2
+    elif operator == 'multiply':
+        return num1 * num2
+    elif operator == 'divide':
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return num1 / num2
+    else:
+        return "invalid input"
 result = perform_operation()
 print(f"Result: {result}")
 
