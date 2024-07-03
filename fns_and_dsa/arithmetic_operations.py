f42ec9847c4c64eac6f8a3fc5bac69ee34ce92a9
tarik <tarikaziz2242@gmail.com>
2024-07-03 05:08:45 -0700

    modifying the full to remove errors

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,25 +1,15 @@
-def perform_operation():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 != 0:
-                return num1 / num2
-            else:
-                print("Cannot divide by zero")
-                return None
-        case _:
-            print("Invalid Operator")
-            return None
-result = perform_operation()
-if result != None:
-    print(f"Result: {result}")
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Cannot divide by zero"
+    else:
+        return "Error: Invalid operation"
 
