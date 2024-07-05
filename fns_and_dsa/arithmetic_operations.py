8a60cd4e3577588056e7866728a99a4502f6cf9a
tarik <tarikaziz2242@gmail.com>
2024-07-05 00:11:45 -0700

    arithmetic operations on funtions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,17 @@
-def perform_operation():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operator = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    if operator == 'add':
-        return num1 + num2
-    elif operator == 'subtract':
-        return num1 - num2
-    elif operator == 'multiply':
-        return num1 * num2
-    elif operator == 'divide':
-        if num2 == 0:
-            return "Cannot divide by zero"
-        else:
-            return num1 / num2
-    else:
-        return "invalid input"
-result = perform_operation()
-print(f"Result: {result}")
-
-
+def perform_operation(num1, num2, operator):
+    match operator:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 != 0:
+                return num1 / num2
+            else:
+                print("Cannot divide by zero")
+                return None
+        case _:
+            print("invalid operation")
+            return None
