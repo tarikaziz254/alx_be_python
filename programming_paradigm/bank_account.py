15bd46c12c074da8c5e53c7b9ba1c5d57946a790
tarik <tarikaziz2242@gmail.com>
2024-07-12 23:17:43 -0700

    updated return keyword in deposit function

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,8 +1,9 @@
 class BankAccount:
-    def __init__(self):
-        self.account_balance = 0
+    def __init__(self,account_balance=0):
+        self.account_balance = account_balance
     def deposit(self,amount):
         self.account_balance += amount
+        return self.account_balance
     def withdraw(self,amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
@@ -10,4 +11,4 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}")
+        print(f"Current balance: ${self.account_balance:.2f}")
