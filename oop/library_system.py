0428610ad8ff339c765c043795c0f87c0eeeb375
tarik <tarikaziz2242@gmail.com>
2024-07-19 00:45:59 -0700

    modification of the str method in classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
@@ -7,33 +7,28 @@ class Book:
         return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
-    def __init__(self,title,author,file_size):
-        super().__init__(title,author) 
+    def __init__(self, title: str, author: str, file_size:int):
+        super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"{super().__str__()} (E-Book,{self.file_size} MB)"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
-    def __init__(self,title,author,page_count):
-        super().__init__(title,author)
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"{super().__str__()}, (Print Book, {self.page_count} pages)"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self,book):
-        if isinstance(book,Book):
-            self.books.append(book)
-        else:
-            raise TypeError("Only instance of Book or its subclasses can be added")
-        
+    def add_book(self, book):
+        self.books.append(book)
+
     def list_books(self):
-        if not self.books:
-            print("No books in the library")
-        else:
-            for book in self.books:
-                print(book)
+        for book in self.books:
+            print(book)
