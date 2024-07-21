695023527b6cdaadbdbac54d30b7b39027d9d3db
tarik <tarikaziz2242@gmail.com>
2024-07-21 05:49:13 -0700

    removed the str method

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,25 +1,23 @@
 import math
+
 class Shape:
+    def __init__(self):
+        pass
+
     def area(self):
-        raise NotImplementedError
-    
+        pass
+
 class Rectangle(Shape):
-    def __init__(self,length,width):
+    def __init__(self, length, width):
         self.length = length
         self.width = width
 
     def area(self):
         return self.length * self.width
-    
-    def __str__(self):
-        f"The area of the Rectangle is: {self.area()}"
 
 class Circle(Shape):
-    def __init__(self,radius):
+    def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius**2)
-    
-    def __str__(self):
-        f"The area of the Circle is: {self.area()}"
+        return  math.pi * (self.radius** 2)        
